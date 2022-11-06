0c4ffade7a5ca6dcb2f3a6eb6fc359f1e3160409
nicksharpe <nickdsharpe@comcast.net>
2022-11-06 14:48:49 -0700

    adding the election audit code.

--- PyPoll.py
+++ PyPoll.py
@@ -5,20 +5,70 @@ import os
 file_to_load = 'election_results.csv'
 file_to_save = os.path.join("analysis", "election_analysis.txt")
 
+# Initialize total vote counter
+total_votes = 0
+candidate_options = []
+candidate_votes = {}
+
+winning_candidate = ""
+winning_count = 0
+winning_percentage = 0
+
 # Open election results and save the file
 with open(file_to_load) as election_data:
 
     # Read the file object with the reader function
     file_reader = csv.reader(election_data)
 
-    # Print the header row
+    # Read the header row
     headers = next(file_reader)
 
-    print(headers)
-    
+    # Increment total votes
+    for row in file_reader:
 
-# 1. Total number of votes cast
-# 2. Complete list of candidates who recieved votes
-# 3. Percentage of votes each candidate won
-# 4. Total number of votes each candidate won
-# 5. Winner of election based on popular vote
+        total_votes += 1
+
+        candidate_name = row[2]
+       
+        if candidate_name not in candidate_options:
+            
+            candidate_options.append(candidate_name)
+
+            candidate_votes[candidate_name] = 0
+
+        candidate_votes[candidate_name] += 1
+
+
+
+# Print total votes
+
+for candidate_name in candidate_votes:
+
+    votes = candidate_votes[candidate_name]
+
+    vote_percentage = float(votes) / float(total_votes) * 100
+
+    print(f"{candidate_name}: {vote_percentage:.1f}% ({votes:,})\n")
+
+    if (votes > winning_count) and (vote_percentage > winning_percentage):
+
+        winning_count = votes
+
+        winning_percentage = vote_percentage
+
+        winning_candidate = candidate_name
+    
+winning_candidate_summary = (
+
+    f"--------------------------\n"
+    f"Winner: {winning_candidate}\n"
+    f"Winning Vote Count: {winning_count:,}\n"
+    f"Winning Percentage: {winning_percentage:.1f}%\n"
+    f"---------------------------\n")
+    
+print(winning_candidate_summary)
+
+        
+
+
+
